38eb757ebea2a3fc9a88b74332c5cf56de5de8d4
sirseb2002 <36109024+sirseb2002@users.noreply.github.com>
2021-03-02 11:19:57 -0500

    Add files via upload
    code de familiarisation (mission 1) pour le MVP (minimum viable product) du STIM kit

---
+++ helloworld_intro.py
@@ -0,0 +1,19 @@
+
+import mcpi.minecraft as minecraft
+import time
+
+mc = minecraft.Minecraft.create()
+
+time.sleep(3)
+
+mc.postToChat("BONJOUR AGENCE SPATIALE !!!")
+
+time.sleep(3)
+
+mc.player.setPos(20, 0, 20)
+
+
+
+	
+
+
