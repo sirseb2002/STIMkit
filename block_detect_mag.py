ba21b81edb37025debffa8892dec5a68ecded992
Sébastien Roy <seb_roy2002@yahoo.ca>
2021-03-12 09:35:38 -0500

    Add files via upload
    Code for mission 2 (MVP) with gold block detection. This uploaded version is a test on X random coordinates only. This needs to be update to bury the gold block @ Y-1 at random, Z=0 and X at random on the Minecraft Pi flatword version for MVP

---
+++ block_detect_mag.py
@@ -0,0 +1,54 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+from random import randint
+from gpiozero import LED, Buzzer
+from time import sleep
+from math import sqrt
+
+mc = minecraft.Minecraft.create()
+
+mc.postToChat("Go find the block")
+
+p = mc.player.getTilePos()
+# x = p.x + randint(-20, 20)
+# y = p.y + randint(-5, 5)
+# z = p.z +randint(-20,20)
+
+x = p.x + randint(-20, 20)
+y = p.y
+z = p.z
+
+mc.setBlock(x, y , z, block.GOLD_BLOCK.id)
+
+led = LED(26)
+buzz = Buzzer(24)
+
+led.on()
+sleep(1)
+led.off()
+
+dist = 0
+gameover = False
+
+while gameover == False:
+    p = mc.player.getTilePos()
+
+    xd = p.x - x
+    yd = p.y - y
+    zd = p.y - z
+
+    #mc.postToChat(xd)
+    #mc.postToChat(yd)
+    #mc.postToChat(zd)
+
+    #dist_now = sqrt((xd*xd) + (yd*yd) + (zd*zd))
+    
+    dist_now = sqrt(xd*xd)
+
+    mc.postToChat(dist_now)
+
+    if dist_now < 5:
+        led.on()
+    else:
+        led.off()
+
