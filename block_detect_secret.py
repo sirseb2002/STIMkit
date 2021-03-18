c8ef6d68f7aa2820107364277fe3fb2a35a0a463
sirseb2002 <36109024+sirseb2002@users.noreply.github.com>
2021-03-18 18:48:33 -0400

    essais de détection buried sur plus grande distance pour réflexion de découverte de code secret

--- block_detect_secret.py
+++ block_detect_secret.py
@@ -5,6 +5,12 @@ from gpiozero import LED
 from time import sleep
 from math import sqrt
 
+led_target = LED(26)
+
+led_target.on()
+sleep(2)
+led_target.off()
+
 mc = minecraft.Minecraft.create()
 
 mc.postToChat("Go find the SECRET CODE !")
@@ -13,12 +19,35 @@ sleep(1)
 mc.player.setPos(1,0,1)
 sleep(0.5)
 
-mc.setBlock(28, 0 , 28, block.IRON.id)
-mc.setBlock(30, 0 , 30, block.SANDSTONE.id)
-mc.setBlock(35, 0 , 35, block.GRAVEL.id)
-mc.setBlock(40, 0 , 40, block.DIRT.id)
-
 p = mc.player.getTilePos()
-# x = p.x + randint(-20, 20)
-# y = p.y + randint(-5, 5)
-# z = p.z +randint(-20,20)
+
+IRx = p.x + randint(26, 100)
+IRy = p.y - 2
+IRz = p.z + randint(26, 100)
+
+SSx = p.x + randint(26, 100)
+SSy = p.y - 2
+SSz = p.z + randint(26, 100)
+
+GRx = p.x + randint(26, 100)
+GRy = p.y - 2
+GRz = p.z + randint(26, 100)
+
+DTx = p.x + randint(26, 100)
+DTy = p.y - 2
+DTz = p.z + randint(26, 100)
+
+
+mc.setBlock(IRx, IRy , IRz, block.IRON.id)
+mc.setBlock(SSx, SSy , SSz, block.SANDSTONE.id)
+mc.setBlock(GRx, GRy , GRz, block.GRAVEL.id)
+mc.setBlock(DTx, DTy , DTz, block.DIRT.id)
+
+dist = 0
+gameover = False
+
+while gameover == False:
+    IRp = mc.player.getTilePos()
+    # x = p.x + randint(-20, 20)
+    # y = p.y + randint(-5, 5)
+    # z = p.z +randint(-20,20)
