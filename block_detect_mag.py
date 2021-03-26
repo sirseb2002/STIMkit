77b3ab41fbb24d1c67334c225010c1e5ff44d741
sirseb2002 <36109024+sirseb2002@users.noreply.github.com>
2021-03-26 09:50:07 -0400

    Caractères incompatibles avec le compilateur Python enlevés (É, ç, ' etc)

--- block_detect_mag.py
+++ block_detect_mag.py
@@ -1,6 +1,6 @@
 # Programme mission 2
 
-#Étape 1
+#Etape 1
 #importez les librairies utiles à la mission 2
 import mcpi.minecraft as minecraft
 import mcpi.block as block
@@ -10,21 +10,21 @@ from time import sleep
 from math import sqrt
 
 
-#Étape 2
+#Etape 2
 #Nommez votre librairie
-#nous le nomons ici "mc"
+#nous le nommons ici "mc"
 mc = minecraft.Minecraft.create()
 
 
-#Étape 3
+#Etape 3
 #Demandons au libraire d'afficher un message sur l'écran
 mc.postToChat("Allez trouver le bloc d'or")
 #Donnons-nous du temps pour le voir
 sleep(1)
 
 
-#Étape 4
-#Désignons les sorties du RaspBerry Pi qui iront vers les DEL
+#Etape 4
+#Designons les sorties du RaspBerry Pi qui iront vers les DEL
 led_target = LED(26)  #Green LED
 
 #Bargraph designation
@@ -41,17 +41,17 @@ led10= LED(21)
 
 
 
-#Étape 5
-#Plaçons notre travailleur à une position connue dans Minecraft
+#Etape 5
+#Placons notre travailleur à une position connue dans Minecraft
 mc.player.setPos(1, 0, 1)
-#Donnons-nous du temps avant de passer à la prochaine étape
+#Donnons-nous du temps avant de passer a la prochaine etape
 sleep(0.5)
 
-#Déposons un une barrière derrière nous qui nous servira de repère
+#Deposons un une barriere derriere nous qui nous servira de repere
 mc.setBlock(0, 0 , 0, block.FENCE.id)
 
 
-#Étape 6 UN PEU DE MATHÉMATIQUE !
+#Etape 6 UN PEU DE MATHEMATIQUE !
 #
 p = mc.player.getTilePos()
 
@@ -60,18 +60,18 @@ y = p.y - 2
 z = p.z + randint(0, 25)
 
 
-#Étape 7 Demandons au libraire de cacher un bloc d'or
+#Etape 7 Demandons au libraire de cacher un bloc d or
 
 mc.setBlock(x, y , z, block.GOLD_BLOCK.id)
 
 
-#Étape 8 Validons que les DEL fonctionnent
+#Etape 8 Validons que les DEL fonctionnent
 led_target.on()
 sleep(3)
 led_target.off()
 
 
-#Étape 9 Partons à la chasse d'un bloc d'or!
+#Etape 9 Partons a la chasse d'un bloc d or!
 
 gameover = False
 
@@ -86,7 +86,7 @@ while gameover == False:
 
     #mc.postToChat(dist_now)
 
-#Étape 10 Affichons notre proximité avec le bloc, d'or!
+#Etape 10 Affichons notre proximite avec le bloc d or!
     if dist_now < 20:
         led1.on()
         if dist_now < 18:
@@ -128,7 +128,7 @@ while gameover == False:
     else:
         led1.off()
 
-#Étape 11 Allumons la DEL VERTE quand on est direct sur le bloc d'or!
+#Etape 11 Allumons la DEL VERTE quand on est direct sur le bloc d or!
     if dist_now == 0:
         led_target.on()
     else:
