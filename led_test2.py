cc3d950d8458ba0a979e52e585a723682cc9cfd1
OuttaStock <81317800+OuttaStock@users.noreply.github.com>
2021-04-23 15:24:50 -0400

    Add files via upload
    This is the .py file to perform an LED test along the X axis in Minecraft. The script tells the LED's to turn on or off after crossing a certain coordinate. There's 3 separate coordinates for 3 individual LED's. With French Commentaries. Note this adds the ability to turn off the LED's if we enter a coordinate lower then specified.

---
+++ led_test2.py
@@ -0,0 +1,50 @@
+#DEL_Test 1: Test de position Minecraft avec plusieurs DEL.
+#Etape 1 Importons nos bibliothèques.
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+from gpiozero import LED
+from time import sleep
+from math import sqrt
+
+#Etape 2 Donnons les désignations de nos DEL's.
+led1= LED(13)
+led2= LED(19)
+led3= LED(26)
+
+
+#Etape 3 Définissons notre position et créons notre point de référence.
+mc = minecraft.Minecraft.create()
+
+
+mc.player.setPos(1, 0, 1)
+
+sleep(0.5)
+
+
+mc.setBlock(0, 0 , 0, block.FENCE.id)
+
+#Etape 4 Faisons en sorte qu'un message affiche les instructions dans le chat.
+mc.postToChat("Teleportez vous !")
+sleep(1)
+
+#Etape 5 Écrivons les fonctions individuelles de vos DEL.
+#Ici, nous indiquerons à nos DEL quand s'allumer ou s'éteindre.
+#Nous obtiendrons également nos données de position actuelles.
+
+gameover = False
+
+while gameover == False:
+    p = mc.player.getTilePos()
+    
+    if p.x > 10:
+        led1.on()
+        if p.x > 15:
+            led2.on()
+            if p.x > 20:
+                led3.on()
+            else:
+                led3.off()
+        else:
+            led2.off()
+    else:
+        led1.off()
