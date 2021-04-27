01acd5b23b6f22fc86f907dba89e6f63c73bd036
OuttaStock <81317800+OuttaStock@users.noreply.github.com>
2021-04-27 14:08:09 -0400

    Update STIMkit.sh
    Edited : To avoid file corruption and removal of files by user and preventing further access to product the following changes were made : 
    made main command to clone instead of pull,
    added line to empty directory before each clone to ensure full files clone.
    added transferworlds.sh code to avoid having multiple files for consumers to click on.

--- STIMkit.sh
+++ STIMkit.sh
@@ -4,4 +4,8 @@ cd ..
 
 cd STIMkit
 
-git pull
+rm -r*
+
+git clone https://github.com/sirseb2002/STIMkit/
+
+sudo cp -r /home/pi/STIMkit/mars_1.5c /home/pi/.minecraft/games/com.mojang/minecraftWorlds/
