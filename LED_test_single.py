ba3dcafffc8e339d8463a8f6fd9d04b0fb18e5d0
Sébastien Roy <sebastien.roy@miroar.ca>
2021-05-19 13:18:41 -0400

    Update LED_test_single.py
    Removed PLATEAU mode in order to use the GPIO number instead of pin number
    replaced pin 8 by GPIO 26

--- LED_test_single.py
+++ LED_test_single.py
@@ -2,14 +2,13 @@ import RPi.GPIO as GPIO # Importer la librairie Raspberry Pi GPIO
 from time import sleep # Importer la fonction SLEEP du module TIME
 
 GPIO.setwarnings(False) # Ignorer les WARNINGS pour le moment
-GPIO.setmode(GPIO.BOARD) # Utiliser le mode PLATEAU de test
 GPIO.setup(26, GPIO.OUT, initial=GPIO.LOW) # Assigner la pin 26 pour être une sortie. Initialiser la sortie à OFF au départ
 
 while True: # Boucle infinie
-    GPIO.output(8, GPIO.HIGH) # mettre à ON
+    GPIO.output(26, GPIO.HIGH) # mettre à ON
     sleep(1) # Attendre 1 seconde
-    GPIO.output(8, GPIO.LOW) # mettre à OFF
+    GPIO.output(26, GPIO.LOW) # mettre à OFF
     sleep(1) # attendre 1 second
     
-    #recommencer la boucle. Presser STOP pour arrêter
+    #recommencer la boucle. Presser STOP dans la console THONNY pour arrêter
  
