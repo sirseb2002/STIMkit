bf4244c86bc7bd506a686ff6fdcb0cef53433395
NewsofPE <79934175+NewsofPE@users.noreply.github.com>
2021-07-14 09:04:03 -0400

    Replaced "boutton" for "bouton"

--- button_blocks.py
+++ button_blocks.py
@@ -32,9 +32,9 @@ var_block3 = 0
 
 run_once = 0
 
-#Ceci dit d'appuyer sur le boutton 4 quand on est directement sur le bloc
+#Ceci dit d'appuyer sur le bouton 4 quand on est directement sur le bloc
 def message():
-    mc.postToChat("Veuillez appuyer sur le boutton 4")
+    mc.postToChat("Veuillez appuyer sur le bouton 4")
 
 #Ceci fait que le bloc 1 ne peut plus apparaître et fait que le bloc suivant peut apparaître
 def end1():
@@ -171,12 +171,12 @@ def distance_now():
         
         if (dist_now == 0) and (yd == 1):
             led_target.on()
-            #Ceci dit d'appuyer sur le boutton 4 quand le joueur est sur le bloc
+            #Ceci dit d'appuyer sur le bouton 4 quand le joueur est sur le bloc
             if run_once == 0:
                 message()
                 run_once = 1
             if button4.is_pressed:
-                print("Boutton 4 est appuyé")
+                print("Bouton 4 est appuyé")
                 gameover = True
         else:
             led_target.off()
@@ -190,13 +190,13 @@ mc.player.setPos(1, 0, 1)
 #Ceci place une clôture sur la coordonée 0, 0, 0
 mc.setBlock(0, 0, 0, block.FENCE.id)
 
-#Ceci dit au joueur d'appuyer sur le boutton 1
-#Quand le boutton 1 est appuyé, cela fait apparaître le premier bloc au hazard
+#Ceci dit au joueur d'appuyer sur le bouton 1
+#Quand le bouton 1 est appuyé, cela fait apparaître le premier bloc au hazard
 #Ensuite ceci dit au joueur d'aller trouver le bloc
 if var_block1 == 0:
-    mc.postToChat("Veuillez appuyer sur le boutton 1")
+    mc.postToChat("Veuillez appuyer sur le bouton 1")
     button1.wait_for_press()
-    print("Boutton 1 est appuyé")
+    print("Bouton 1 est appuyé")
     p = mc.player.getTilePos()
     x = p.x + randint(0, 25)
     y = p.y - 2
@@ -207,12 +207,12 @@ if var_block1 == 0:
     end1()
 
 #Ceci dit au joueur d'appuyer sur le boutton 2
-#Quand le boutton 2 est appuyé, cela fait apparaître le deuxième bloc au hazard
+#Quand le bouton 2 est appuyé, cela fait apparaître le deuxième bloc au hazard
 #Ensuite ceci dit au joueur d'aller trouver le bloc
 if (var_block1 == 1) and (var_block2 == 0):
-    mc.postToChat("Veuillez appuyer sur le boutton 2")
+    mc.postToChat("Veuillez appuyer sur le bouton 2")
     button2.wait_for_press()
-    print("Boutton 2 est appuyé")
+    print("Bouton 2 est appuyé")
     p = mc.player.getTilePos()
     x = p.x + randint(0, 25)
     y = p.y - 2
@@ -223,12 +223,12 @@ if (var_block1 == 1) and (var_block2 == 0):
     end2()
 
 #Ceci dit au joueur d'appuyer sur le boutton 3
-#Quand le boutton 3 est appuyé, cela fait apparaître le troisième bloc au hazard
+#Quand le bouton 3 est appuyé, cela fait apparaître le troisième bloc au hazard
 #Ensuite ceci dit au joueur d'aller trouver le bloc
 if (var_block1 == 1) and (var_block2 == 1) and (var_block3 == 0):
-    mc.postToChat("Veuillez appuyer sur le boutton 3")
+    mc.postToChat("Veuillez appuyer sur le bouton 3")
     button3.wait_for_press()
-    print("Boutton 3 est appuyé")
+    print("Bouton 3 est appuyé")
     p = mc.player.getTilePos()
     x = p.x + randint(0, 25)
     y = p.y - 2
